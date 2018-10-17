f8e9d2a4810fae9ec31cdf0cb080fa3394f3aba8
DanAlipes <dan@alipes.com>
2018-10-17 17:15:04 -0400

    setup pages

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>hello</title>
+    <script src="{{ mix('/js/app.js') }}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
