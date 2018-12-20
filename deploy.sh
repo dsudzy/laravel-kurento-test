57541f2b8bcf37d035867f9518912193dd7477cf
DanAlipes <dan@alipes.com>
2018-12-20 11:36:16 -0500

    add to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,13 @@
 FILE="$1"
 COMMAND="$2"
 
-echo "sudo cp -rf ./* /var/www/html/${FILE}"
-sudo cp -rf ./* /var/www/html/${FILE}
+if [ -n "$FILE" ]; then
+    echo "sudo cp -rf ./$FILE /var/www/html/"
+    sudo cp -rf ./$FILE /var/www/html/
+else
+    echo "sudo cp -rf ./* /var/www/html/"
+    sudo cp -rf ./* /var/www/html/
+fi
 
 echo "$COMMAND"
 $COMMAND
