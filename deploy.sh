2a5a814f7649a06462ac750f60478f7aec37c3e3
Dan <dsudenfield@gmail.com>
2018-12-21 18:48:31 -0500

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ FILE="$1"
 COMMAND="$2"
 
 if [ -n "$FILE" ]; then
-    echo "sudo cp -rf ./$FILE /var/www/html/"
-    sudo cp -rf ./$FILE /var/www/html/
+    echo "sudo cp -rf ./$FILE /var/www/html/$FILE"
+    sudo cp -rf ./$FILE /var/www/html/$FILE
 else
     echo "sudo cp -rf ./* /var/www/html/"
     sudo cp -rf ./* /var/www/html/
