3ab9a3e7ba798b5a94b4f055d43a781a06da58ba
DanAlipes <dan@alipes.com>
2018-12-21 13:40:08 -0500

    add new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-COMMAND="$1"
+FILE="$1"
+COMMAND="$2"
 
-echo "sudo cp -rf ./* /var/www/html/"
-sudo cp -rf ./* /var/www/html/
+if [ -n "$FILE" ]; then
+    echo "sudo cp -rf ./$FILE /var/www/html/"
+    sudo cp -rf ./$FILE /var/www/html/
+else
+    echo "sudo cp -rf ./* /var/www/html/"
+    sudo cp -rf ./* /var/www/html/
+fi
 
 echo "$COMMAND"
 $COMMAND
